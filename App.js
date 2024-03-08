d0fb07b1ea4a4c87ae38a7e3740d600e05abe018
ugurakcora <uakcora97@gmail.com>
2024-03-08 19:30:43 +0300

    created screens

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import IndexScreen from "./screens/indexScreen";
+import CreateScreen from "./screens/CreateScreen";
+
+const stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <stack.Navigator screenOptions={{ headerTitle: "Blog" }}>
+        <stack.Screen name="Index" component={IndexScreen} />
+        <stack.Screen name="Create" component={CreateScreen} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
