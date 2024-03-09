731bdb7f8b67ca15761a9a5b035adf47f16a9d8e
ugurakcora <uakcora97@gmail.com>
2024-03-09 20:49:27 +0300

    added context

--- App.js
+++ App.js
@@ -3,18 +3,21 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import IndexScreen from "./screens/indexScreen";
+import IndexScreen from "./screens/IndexScreen";
 import CreateScreen from "./screens/CreateScreen";
+import { BlogProvider } from "./context/blogContext";
 
 const stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <stack.Navigator screenOptions={{ headerTitle: "Blog" }}>
-        <stack.Screen name="Index" component={IndexScreen} />
-        <stack.Screen name="Create" component={CreateScreen} />
-      </stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <stack.Navigator screenOptions={{ headerTitle: "Blog" }}>
+          <stack.Screen name="Index" component={IndexScreen} />
+          <stack.Screen name="Create" component={CreateScreen} />
+        </stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
