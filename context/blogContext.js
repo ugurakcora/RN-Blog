4a751da206b6532b6bd13e933b57acc3636111ff
ugurakcora <uakcora97@gmail.com>
2024-03-12 16:38:46 +0300

    added update page

--- context/blogContext.js
+++ context/blogContext.js
@@ -13,6 +13,16 @@ const blogReducer = (state, action) => {
       ];
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id
+          ? {
+              ...blogPost,
+              title: action.payload.title,
+              content: action.payload.content,
+            }
+          : blogPost;
+      });
     default:
       return state;
   }
@@ -33,8 +43,17 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   []
 );
